224c68512c5e0ef92a38dcba66b24ba839d3de57
charlesagyemang <micnkru@gmail.com>
2019-08-14 02:43:24 +0000

    Feat: Rspec refactor

--- follow_rule/test.rb
+++ follow_rule/test.rb
@@ -5,43 +5,38 @@ require './mailer.rb'
 
 describe 'Invoice' do
 
-	it 'should create an invoice successfully' do 
-		invoice = Invoice.new("Charles", "AZ", 1000)
-		expect(invoice.get_customer).to eq("Charles")
-	end		
+	#GLobal rspec test variables
+	let(:invoice)    { Invoice.new("Charles", "AZ", 1000) }
+	let(:sales_tax)  { SalesTax.new(invoice.state)        }
+	let(:total_cost) { invoice.total_cost(sales_tax.tax)  }
+	let(:email)      { Mailer.new(invoice.details)        }
 
-	it 'should give details of invoice' do 
-		invoice = Invoice.new("Charles", "AZ", 1000)
+	it 'should create an invoice successfully' do
+		expect(invoice.customer).to eq("Charles")
+	end
+
+	it 'should give details of invoice' do
 		expect(invoice).to respond_to(:details)
-	end	
+	end
 
-	it 'should not have a email_invoice method that sends emails' do 
-		invoice = Invoice.new("Charles", "AZ", 1000)
+	it 'should not have a email_invoice method that sends emails' do
 		expect(invoice).not_to respond_to(:email_invoice)
-	end	
+	end
 
-	it 'should not have a email_invoice method that sends emails' do 
-		invoice = Invoice.new("Charles", "AZ", 1000)
+	it 'should not have a email_invoice method that sends emails' do
 		expect(invoice).not_to respond_to(:sales_tax)
-	end	
+	end
 
 
-	it 'should return the correct value for state tax successfully' do 
-		invoice = Invoice.new("Charles", "AZ", 1000)
-		sales_tax = SalesTax.new(invoice.get_state)
+	it 'should return the correct value for state tax successfully' do
 		expect(sales_tax.tax).to eq(5.5)
 	end
 
-	it 'should return the correct total_cost successfully' do 
-		invoice = Invoice.new("Charles", "AZ", 1000)
-		sales_tax = SalesTax.new(invoice.get_state)
-		total_cost = invoice.total_cost(sales_tax.tax)
+	it 'should return the correct total_cost successfully' do
 		expect(total_cost).to eq(1005.5)
 	end
 
-	it 'should send an email successfully with the details of the invoice' do 
-		invoice = Invoice.new("Charles", "AZ", 1000)
-		email = Mailer.new(invoice.details)
+	it 'should send an email successfully with the details of the invoice' do
 		expect(email).to respond_to(:email_invoice)
 	end
 
